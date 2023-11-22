b4d57c27f724a5d583c210405046d591eecb4a5c
Gabriel Díaz Arevalillo <gabriel.diazarevalillo@usp.ceu.es>
2023-11-22 08:39:19 +0100

    part3

---
+++ src/dominio/Vehiculo.java
@@ -0,0 +1,12 @@
+package dominio;
+
+public class Vehiculo {
+    String marca;
+    String modelo;
+    double precioBase;
+
+    public String toString(){
+        return "Marca: "+marca+" Modelo: "+modelo+" Precio Base: "+precioBase;
+    }
+
+}
